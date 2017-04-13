8bf1b07b9984ae253c797d3c7ce5ca3aa849fed6
slowmoyang <slowmoyang@gmail.com>
2017-04-13 17:15:44 +0900

    ewwww

---
+++ convnet/jet_with_placeholder.py
@@ -0,0 +1,77 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from reader import inputs
+import convnet
+from drawer import get_date
+from layer import convolutional_layer, fully_connected_layer 
+
+NUM_EPOCHS = 100
+
+with tf.Graph().as_default():
+    with tf.name_scope('input'):
+        images = tf.placeholder(dtype=tf.float32, shape=[None, 33, 33, 3], name='images')
+        labels = tf.placeholder(dtype=tf.int64, shape=[None, 2], name='labels')
+#        images, labels = inputs(filename = '../data/tfrecords/jet01.tfrecords',
+#                                batch_size = 5,
+#                                num_epochs = NUM_EPOCHS)
+    with tf.name_scope('dropout'):
+        keep_prob = tf.placeholder(tf.float32)
+
+    logits = convnet.inference(images, keep_prob)
+
+    loss = convnet.loss(logits, labels)
+
+    accuracy = convnet.evaluation(logits, labels)
+
+    train_op = convnet.training(loss, learning_rate=0.001)
+
+    init_op = tf.group(tf.global_variables_initializer(),
+                       tf.local_variables_initializer())
+
+    # Create a session for running operations in the Graph.
+    sess = tf.Session()
+
+    merged = tf.summary.merge_all()
+    log_path = '/tmp/kps/jet_logs_' + get_date()
+    train_writer = tf.summary.FileWriter(log_path + '/train', sess.graph)
+    test_writer = tf.summary.FileWriter(log_path + '/test')
+
+    sess.run(init_op)
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    try:
+        step = 0
+        while not coord.should_stop():
+            start_time = time.time()
+            # training
+            summary, _ = sess.run([merged, train_op], feed_dict={keep_prob: 0.5})
+            train_writer.add_summary(summary, step)
+
+            duration = time.time() - start_time
+
+            # validation
+            if step % 100 == 0:
+                summary, acc_value, loss_value = sess.run([merged, accuracy, loss], feed_dict={keep_prob: 1.0})
+                test_writer.add_summary(summary, step)
+                print('Step %d: loss = %.2f, accuracy = %.3f (%.3f sec)' % (step, loss_value, acc_value, duration))
+            step += 1
+    except tf.errors.OutOfRangeError:
+        print('Done training for %d epochs, %d steps.' % (NUM_EPOCHS, step))
+    finally:
+        train_writer.close()
+        test_writer.close()
+        # When done, ask the threads to stop.
+        coord.request_stop()
+ 
+    # Wait for threads to finish.
+    coord.join(threads)
+    sess.close()
---
+++ convnet/layer.py
@@ -0,0 +1,75 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+'''
+/www.tensorflow.org/get_started/summaries_and_tensorboard This Variable will hold the state of the weights for the layer
+'''
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def xavier_init(name, shape, is_conv2d=True):
+    if is_conv2d:
+        weight = tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer_conv2d())
+    else:
+        weight = tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
+    return weight 
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def variable_summaries(var):
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+def convolutional_layer(input_tensor, kernel_shape, layer_name):
+    with tf.name_scope(layer_name):
+        # Convolution stage: Affine transform
+        with tf.name_scope('kernel'):
+            kernel_name = 'kernel_xavier_' + layer_name
+            kernel = xavier_init(name=''+layer_name, shape=kernel_shape, is_conv2d=True) 
+            variable_summaries(kernel)
+        with tf.name_scope('convolution'):
+            conv = tf.nn.conv2d(input_tensor, kernel, strides=[1, 1, 1, 1], padding='SAME')
+            variable_summaries(conv)
+        with tf.name_scope('biases'):
+            output_channel = kernel_shape[-1]
+            biases = bias_variable([output_channel])
+        with tf.name_scope('plus_biases'):
+            preactivate = tf.nn.bias_add(conv, biases)
+        # Detector stage: Nonlinearity (e.g. rectified linear unit)
+        with tf.name_scope('activations'):
+            activations = tf.nn.relu(preactivate)
+        # Pooling stage 
+        with tf.name_scope('max_pool'):
+            pool = tf.nn.max_pool(activations, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        return pool
+
+def fully_connected_layer(input_tensor, input_dim, output_dim, layer_name, act_ftn=tf.nn.relu):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            weights = xavier_init(name='fc'+layer_name, shape=[input_dim, output_dim], is_conv2d=False)
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([output_dim])
+            variable_summaries(biases)
+        with tf.name_scope('affine_transformation'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram('pre_activations', preactivate)
+        activations = act_ftn(preactivate, name='activation')
+        tf.summary.histogram('activations', activations)
+        return activations
+
+def weird_layer():
+    pass
